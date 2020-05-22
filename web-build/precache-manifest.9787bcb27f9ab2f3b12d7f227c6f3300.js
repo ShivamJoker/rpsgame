16342b5ca3efe6e43934dcf4dae7ff29d1e9e333
Shivam <SINGHSHIVAMKR@GMAIL.COM>
2020-05-22 16:58:36 +0530

    added web build

---
+++ web-build/precache-manifest.9787bcb27f9ab2f3b12d7f227c6f3300.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4758f840893c5b46cc16",
+    "url": "/static/js/app.bdc8efd7.chunk.js"
+  },
+  {
+    "revision": "e91487e8c8aa62ce91ce",
+    "url": "/static/js/runtime~app.34c76111.js"
+  },
+  {
+    "revision": "9a6bb16c6cba63aa8dba",
+    "url": "/static/js/2.a60a3540.chunk.js"
+  },
+  {
+    "revision": "6c2a82f7c36a9279422113556107a710",
+    "url": "/static/media/rock.6c2a82f7.png"
+  },
+  {
+    "revision": "293adbc7267032bbda26eff114d049b2",
+    "url": "/static/media/scissors.293adbc7.png"
+  },
+  {
+    "revision": "375c7c6a134aa7055de646d47c2c9eec",
+    "url": "/static/media/paper.375c7c6a.png"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "8eada1c856ce8a19f47fe3945aaebfaf",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "be5cd433af5963c0063cdad2185cdf02",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "d3ba57373975008dc2497c2d9be6ece8",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "8d76e3dcdcf47454a2c77d36e423fc7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9292fe017217ab3e50087953c12977fa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
